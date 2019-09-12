de4d1aa1b5510ae6590724bbfee3c7dd1295f296
Audrey Garcia <audrey.marcella9@gmail.com>
2019-09-12 17:15:42 -0600

    keep track of wins losses and ties

--- src/App.js
+++ src/App.js
@@ -10,10 +10,20 @@ class App extends React.Component {
     choices: [ "rock", "paper", "scissors"],
     userChoice: "",
     compChoice: "",
+    wins: 0,
+    losses: 0,
+    ties: 0,
   };
 
   choose = (choice) => {
-    this.setState({ choices: this.state.choices,  userChoice: choice, compChoice: this.state.compChoice, });
+    this.setState({ 
+      choices: this.state.choices,  
+      userChoice: choice, 
+      compChoice: this.state.compChoice, 
+      wins: this.state.wins,
+      losses: this.state.losses,
+      ties: this.state.ties,
+    });
   };
 
   chosenImg = (choice) => {
@@ -26,20 +36,51 @@ class App extends React.Component {
 
   setCompChoice = () => {
     var choice = this.state.choices[Math.floor(Math.random() * 3)];
-    this.setState({ choices: this.state.choices,  userChoice: this.state.userChoice, compChoice: choice, });
+    this.setState({ 
+      choices: this.state.choices,  
+      userChoice: this.state.userChoice, 
+      compChoice: choice,
+      wins: this.state.wins,
+      losses: this.state.losses,
+      ties: this.state.ties,
+    });
   };
 
   win = () => {
+    this.setState({ 
+      choices: this.state.choices,  
+      userChoice: this.state.userChoice, 
+      compChoice: this.state.choice,
+      wins: (this.state.wins + 1),
+      losses: this.state.losses,
+      ties: this.state.ties,
+    });
     alert("You win!")
   }
 
   lose = () => {
+    this.setState({ 
+      choices: this.state.choices,  
+      userChoice: this.state.userChoice, 
+      compChoice: this.state.choice,
+      wins: this.state.wins,
+      losses: (this.state.losses + 1),
+      ties: this.state.ties,
+    });
     alert("You lose!")
   }
 
   result = () => {
     switch(this.state.userChoice) {
       case this.state.compChoice:
+        this.setState({ 
+          choices: this.state.choices,  
+          userChoice: this.state.userChoice, 
+          compChoice: this.state.choice,
+          wins: this.state.wins,
+          losses: this.state.losses,
+          ties: (this.state.ties + 1),
+        });
         alert("It's a tie!");
         break;
       case "rock":
@@ -79,26 +120,38 @@ class App extends React.Component {
 
   render() {
     return (
-      <>
+      <div>
         <h1>Rock Paper Scissors</h1>
+        <h3>Make your choice:</h3>
         <div>
           <Choice name="rock" img={rock} choose={this.choose} />
           <Choice name="paper" img={paper} choose={this.choose} />
           <Choice name="scissors" img={scissors} choose={this.choose} />
         </div>
-        <div className="versus">
+        <div className="flex">
           <div>
-            <h2>Your choice:</h2>
+            <h2>You:</h2>
             <Choice name={this.state.userChoice} img={this.chosenImg(this.state.userChoice)} />
           </div>
-          <h2>VS</h2>
+          <h3>VS</h3>
           <div>
-            <h2>Computer choice:</h2>
+            <h2>Computer:</h2>
             <Choice name={this.state.compChoice} img={this.chosenImg(this.state.compChoice)} />
           </div>
         </div>
         <button onClick={() => this.play()}>Shoot</button>
-      </>
+        <div className="flex">
+          <div>
+            Wins: {this.state.wins}
+          </div>
+          <div>
+            Losses: {this.state.losses}
+          </div>
+          <div>
+            Ties: {this.state.ties}
+          </div>
+        </div>
+      </div>
     )
   }
 };
